2f7239b4b2be307755245ea7de941b91b87c0436
Peninah Anyango <pennie.yna@gmail.com>
2017-05-17 11:29:14 +0300

    Edit argument

--- people_model.py
+++ people_model.py
@@ -7,7 +7,7 @@ class Person(object):
 
     def __init__(self, first_name, last_name, title, wants_accomodation = "N"):
 
-        self.name = first_name + spacer + last_name
+        self.person_name = first_name + spacer + last_name
         self.title = title  
 
     def __repr__(self):
