8815d0a42eca7d87e7564b2bf45cec2e5744cf1e
Peninah Anyango <pennie.yna@gmail.com>
2017-05-17 10:34:45 +0300

    Add classes

---
+++ room_model.py
@@ -0,0 +1,38 @@
+spacer = " "
+
+
+class Room(object):
+
+    """ Office and Living space inherits from the Class Room """
+    
+    def __init__(self, room_name, room_type):
+        self.occupants = []
+        self.room_name = room_name
+        self.room_type = room_type
+
+    def __repr__(self):
+        return "<Room %s %s>" % self.room_name, self.room_type
+
+
+class Office(Room):
+    
+    capacity = 6
+
+    def __init__(self, room_name, room_type):
+        super(Office, self).__init__(room_name, room_type)
+
+    def __repr__(self):
+        return "<Office %s %s>" % (self.room_name, self.room_type)
+
+
+class Living(Room):
+
+    capacity = 4
+
+    def __init__(self, room_name, room_type):
+        super(Living, self).__init__(room_name, room_type)
+
+    def __repr__(self):
+        return "<Living space %s %s>" % (self.room_name, self.room_type)
+
+    
